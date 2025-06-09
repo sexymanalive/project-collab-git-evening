b4aa0a0af0ff065e9640e28f3edf1265a6499ba3
sexymanalive <lyvanna544@gmail.com>
2025-06-09 12:58:03 +0000

    adding student class

--- studentDemo.cpp
+++ studentDemo.cpp
@@ -1,5 +1,10 @@
 #include<iostream> 
 usign namespace std; 
+class Student {
+    private: 
+        string name; 
+        string gender; 
+}; 
 int main(){
     cout<<"This is the code for demo the student class!"<<endl; 
     return 0; 
